0ff41ced84a6115e499c2574c9d30354f3a7d5df
Taras Kozlovskyi <ktarasz@quintagroup.com>
2014-02-21 14:15:32 +0000

    added comments count on blog entry view

--- ftw/blog/browser/entry.py
+++ ftw/blog/browser/entry.py
@@ -1,8 +1,10 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.discussion.interfaces import IConversation
 from ftw.blog.interfaces import IBlogEntryView
 from ftw.tagging.interfaces.tagging import ITagRoot
 from ftw.tagging.utils import getInterfaceRoot
+from zope.component import getMultiAdapter
 from zope.interface import implements
 
 
@@ -26,3 +28,12 @@ class BlogEntryView(BrowserView):
 
     def show_lead_image(self):
         return self.context.getLeadimage() and self.context.showLeadImage()
+
+    def comments_enabled(self):
+        conversation = getMultiAdapter((self.context, self.request),
+                                       name='conversation_view')
+        return conversation.enabled()
+
+    def amount_of_replies(self):
+        conversation = IConversation(self.context)
+        return len([thread for thread in conversation.getThreads()])
